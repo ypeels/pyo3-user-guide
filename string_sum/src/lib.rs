e022838b7d507effca76e0290134a2b54e2db2db
unknown <ypeels@users.noreply.github.com>
2025-12-02 09:34:26 -0500

    comment

--- string_sum/src/lib.rs
+++ string_sum/src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 /// Python module implemented in Rust - must match `lib.name` in Cargo.toml
 // with random fixes from https://stackoverflow.com/questions/79819313/no-method-named-add-function-found-for-reference-pyo3typespymodule
 #[pymodule]
-#[pyo3(name = "custom_name")]
+#[pyo3(name = "custom_name")]  // this would have to match Cargo.toml (note it also must come after the #[pymodule] macro)
 fn string_sum(_py: Python, m: Bound<'_, PyModule>) -> PyResult<()> {//&PyModule) -> PyResult<()> {
 	m.add_function(wrap_pyfunction!(sum_as_string, &m)?)?;
 	
