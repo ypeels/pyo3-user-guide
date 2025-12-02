d5ad68dd83f45c7f302ee97f1eeedc6274d9b16d
unknown <ypeels@users.noreply.github.com>
2025-12-02 08:56:05 -0500

    updates to 0.27.2 example from API docs. ANOTHER QUESTION MARK!?

--- hello_python_from_rust/src/main.rs
+++ hello_python_from_rust/src/main.rs
@@ -1,17 +1,21 @@
+// readme/main page is out of date?
+// https://docs.rs/pyo3/latest/pyo3/#example-using-python-from-rust
+// retrieved for 0.27.2
+
 use pyo3::prelude::*;
 use pyo3::types::IntoPyDict;
+use pyo3::ffi::c_str;
 
 fn main() -> PyResult<()> {
     println!("Hello, world!");
 
-    Python::with_gil(|py| {
+    Python::attach(|py| {
         let sys = py.import("sys")?;
         let version: String = sys.getattr("version")?.extract()?;
         
-        let locals = [("os", py.import("os")?)].into_py_dict(py);
-        let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
-        //let user: String = py.eval(code, None, Some(&locals))?.extract()?;
-        let user = "fake user";
+        let locals = [("os", py.import("os")?)].into_py_dict(py)?;
+        let code = c_str!("os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'");
+        let user: String = py.eval(code, None, Some(&locals))?.extract()?;
 
         println!("Hello {}, I'm Python {}", user, version);
         Ok(())
