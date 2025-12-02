01f409b3c6cd0fc34cc71e6301df4d61056cdba0
unknown <ypeels@users.noreply.github.com>
2025-12-02 08:45:58 -0500

    fixes sys/version

--- hello_python_from_rust/src/main.rs
+++ hello_python_from_rust/src/main.rs
@@ -5,14 +5,15 @@ fn main() -> PyResult<()> {
     println!("Hello, world!");
 
     Python::with_gil(|py| {
-        let sys = py.import("sys");//?);
+        let sys = py.import("sys")?;
         let version: String = sys.getattr("version")?.extract()?;
-
+        
         let locals = [("os", py.import("os")?)].into_py_dict(py);
         let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
-        let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+        //let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+        let user = "fake user";
 
-        println!("Hello {}, I'm Python {}'", user, version);
+        println!("Hello {}, I'm Python {}", user, version);
         Ok(())
     })
 }
