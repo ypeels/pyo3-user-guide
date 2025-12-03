c8bb755a6f84d9902b98ee0bcf9b40f1ee37b1de
unknown <ypeels@users.noreply.github.com>
2025-12-03 10:28:28 -0500

    dict example

--- conversions/src/main.rs
+++ conversions/src/main.rs
@@ -1,6 +1,6 @@
+/*
 use pyo3::prelude::*;
 use pyo3::ffi::c_str;
-
 #[derive(FromPyObject)]
 struct RustyStruct {
     my_string: String,
@@ -21,12 +21,42 @@ class Foo:
             c_str!(""),
             c_str!(""),
         )?;
-
+        
         let class = module.getattr("Foo")?;
         let instance = class.call0()?;
         let rustystruct: RustyStruct = instance.extract()?;
         assert_eq!(rustystruct.my_string, "test");
         println!("rustystruct.my_string = {}", rustystruct.my_string);
+        //println!("rustystruct.__class__.__name__ = {}", rustystruct.__class__.__name__); // fails
         Ok(())
     })
 }
+*/
+
+
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+
+#[derive(FromPyObject)]
+struct RustyStruct {
+    #[pyo3(item("blah"))]
+    my_string: String,
+
+    #[pyo3(item)]
+    my_int: i32,
+}
+
+fn main() -> PyResult<()> {
+    Python::attach(|py| -> PyResult<()> {
+        let dict = PyDict::new(py);
+        dict.set_item("my_int", 32)?;
+        dict.set_item("blah", "test")?;
+        let rustystruct: RustyStruct = dict.extract()?;
+        
+        assert_eq!(rustystruct.my_string, "test");
+        println!("rustystruct.my_string = {}", rustystruct.my_string);
+        println!("rustystruct.my_int = {}", rustystruct.my_int);
+        Ok(())
+    })
+}
+
